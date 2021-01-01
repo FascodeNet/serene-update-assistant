6d4a58b131976771954462a67d17e1bc3cace0d8
kokkiemouse <kokkiemouse@fascode.net>
2021-01-01 16:42:28 +0900

    test script
    Signed-off-by: kokkiemouse <kokkiemouse@fascode.net>

--- cui/src/main.cpp
+++ cui/src/main.cpp
@@ -1,4 +1,50 @@
 #include <serene_updater.hpp>
+#include <unistd.h>
+#include <stdio.h>
 int main(int argc,char* argv[]){
-    serene_updater serene_updaterkun("release");
+    int i,opt;
+    opterr=0;
+    bool isyes=false;
+    bool istest=false;
+    while((opt = getopt(argc,argv,"yt")) != -1){
+        switch(opt){
+            case 'y':
+                isyes=true;
+                break;
+            case 't':
+                istest=true;
+                break;
+            default:
+                std::cout << "Usage " << argv[0] << "[-t] [-y] " << std::endl;
+                break;
+        }
+    }
+    String channel_name;
+    if(istest){
+        channel_name="release_testing";
+    }else
+    {
+        channel_name="release";
+    }
+    
+    serene_updater serene_updaterkun(channel_name);
+    if(serene_updaterkun.isneed_update()){
+        std::cout << "you need update" << std::endl;
+    }else{
+        std::cout << "no update" << std::endl;
+        return 0;
+    }
+    
+    if(!isyes){
+        while(true){
+            std::cout << "Update? [y/N]:" << std::flush;
+            char c=getchar();
+            if(c == 'y')
+                break;
+            return 0;
+        }
+    }
+    serene_updaterkun.update();
+
+
 }
--- lib/src/serene_updater.cpp
+++ lib/src/serene_updater.cpp
@@ -21,6 +21,24 @@ size_t serene_updater_name::buffer_w(char* ptr,size_t size,size_t nmemb,void* st
     }
     return block;
 }
+size_t serene_updater_name::writer_curl(char* ptr,size_t size,size_t nmemb,void* stream){
+    curl_buffer * buf=(curl_buffer*)stream;
+    int block=size * nmemb;
+    if(!buf){
+        return block;
+    }
+    if(!buf->data){
+        buf->data=(char*)malloc(block);
+    }
+    else{
+        buf->data=(char*)realloc(buf->data,buf->size+block);
+    }
+    if(buf->data){
+        memcpy(buf->data+buf->size,ptr,block);
+        buf->size += block;
+    }
+    return block;
+}
 bool serene_updater::isneed_update(){
     int current=atoi(current_version.c_str());
     String download_url="https://repo.dyama.net/new_serene_updater/latest_" + current_channel;
@@ -36,10 +54,12 @@ bool serene_updater::isneed_update(){
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION,serene_updater_name::buffer_w);
     curl_easy_perform(curl);
     curl_easy_cleanup(curl);
-    String new_ver=String(buf->data);
+    String nnext_version=buf->data;
+    next_version=std::regex_replace(nnext_version,std::regex("\n"),"");
     free(buf->data);
     free(buf);
-    int new_v=atoi(new_ver.c_str());
+    int new_v=atoi(next_version.c_str());
+    newv=new_v;
     return current < new_v;
 
 }
@@ -59,4 +79,54 @@ String serene_updater::get_current_version(){
         }
     }
     return "FAILED!";
+}
+int serene_updater::update(){
+    std::cout << "Updating " << std::endl;
+    String download_sh_url="https://repo.dyama.net/new_serene_updater/updater/" + std::to_string(newv);
+    //String download_sh_url="https://repo.dyama.net/new_serene_updater/latest_release";
+    std::cout << download_sh_url << std::endl;
+    CURL *curl2;
+    curl2=curl_easy_init();
+    curl_easy_setopt(curl2,CURLOPT_URL,download_sh_url.c_str());
+    curl_easy_setopt(curl2, CURLOPT_SSL_VERIFYPEER, 0);
+    char tmp_kun[]="/tmp/serene_updateXXXXXX";
+    int download_file_f=mkstemp(tmp_kun);
+    String tmp_file_path="/proc/" + std::to_string(getpid()) + "/fd/" + std::to_string(download_file_f) ;
+    std::ofstream out_streamkun(tmp_file_path,std::ios::out | std::ios::binary);
+    umask(022);
+    serene_updater_name::curl_buffer *buf;
+    buf=(serene_updater_name::curl_buffer*)malloc(sizeof(serene_updater_name::curl_buffer));
+    buf->data=NULL;
+    buf->size=0;
+    curl_easy_setopt(curl2, CURLOPT_WRITEDATA, buf);
+    curl_easy_setopt(curl2, CURLOPT_WRITEFUNCTION,serene_updater_name::buffer_w);
+    curl_easy_perform(curl2);
+    curl_easy_cleanup(curl2);
+    out_streamkun.write(buf->data,buf->size);
+    out_streamkun.flush();
+    free(buf->data);
+    free(buf);
+    //std::cout << execl(tmp_file_path.c_str(),tmp_file_path.c_str())  << std::endl;
+    out_streamkun.close();
+    close(download_file_f);
+    umask(000);
+    chmod(tmp_kun,0777);
+    int child;
+    int status;
+    if((child=fork()) < 0){
+        std::cerr << "fork err" << std::endl;
+        unlink(tmp_kun);
+        return -8;
+    }
+    if(child == 0 ){
+        execl(tmp_kun,tmp_kun,NULL);
+    }else{
+        if(wait(&status) < 0){
+            std::cerr << "wait" << std::endl;
+            unlink(tmp_kun);
+            return -7;
+        }
+    };
+    unlink(tmp_kun);
+    return 0;
 }
